0d32bf0c94da8ae82d547829913514cb3c24a131
Dan Chin <danielmchin@outlook.com>
2021-01-26 12:28:23 -0500

    set redux boilerplate

---
+++ react-app/src/store/session.js
@@ -0,0 +1,15 @@
+const initialState = {
+
+};
+
+const sessionReducer = (state = initalState, action) => {
+  let newState;
+  switch (action.type) {
+    default:
+        return state;
+  }
+};
+
+
+
+export default sessionReducer;
