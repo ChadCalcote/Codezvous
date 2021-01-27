adc9a2f93924bab3f35b71cce9982bc5e492f271
Dan Chin <danielmchin@outlook.com>
2021-01-27 16:35:58 -0500

    updated groups_routes with more routes per front-end

--- app/api/groups_routes.py
+++ app/api/groups_routes.py
@@ -12,11 +12,20 @@ def groups():
     groups = Group.query.all()
     return jsonify([group.to_dict() for group in groups])
 
+
 # Retrieve a single group
 @groups_routes.route('/<int:id>')
 def group(id):
     group = Group.query.get(id)
     return group.to_dict()
+
+
+# Retrieve all events of a group
+@groups_routes.route('/<int:id>/events')
+def group_events(id):
+    group_events = Event.query.filter(Event.group_id == id)
+    return jsonify([group_event.to_dict() for group_event in group_events])
+
 
 # Retrieve a group leader
 @groups_routes.route('/<int:id>/leader')
@@ -24,11 +33,13 @@ def get_leader(id):
     leader_id = User.query.join(Group).filter(Group.id == id).filter(User.id == Group.leader_id).all()
     return jsonify([leader.to_dict() for leader in leader_id])
 
+
 # Retrieve all group members
 @groups_routes.route('/<int:id>/members')
 def get_members(id):
     members = User.query.join(Users_Group).filter(Users_Group.group_id == id).all()
     return jsonify([member.to_dict() for member in members])
+
 
 # Create a group
 @groups_routes.route('/', methods=["POST"])
@@ -70,27 +81,15 @@ def put(id):
 
     return {"message": "success"}
 
+
 # Delete a group
-
-
 @groups_routes.route('/<int:id>', methods=["DELETE"])
 @login_required
 def delete(id):
     events = Event.query.filter(Event.group_id == id).all()
     group = Group.query.get(id)
 
-    # attempted resolving: null value in column "group_id" of relation "events" violates not-null constraint
-    # db.session.delete(events) # Class 'builtins.list' is not mapped ? mapping error
     db.session.delete(group)
     db.session.commit()
 
     return {"message": "success"}
-
-
-# Delete a group
-# @groups_routes.route('/groups/<int:id>', methods=['DELETE'])
-# def delete_group():
-#     group = Group.query.get_or_404(id)
-#     db.session.delete(group)
-#     db.session.commit()
-#     return "success"  # {'message': 'success'} per api?
