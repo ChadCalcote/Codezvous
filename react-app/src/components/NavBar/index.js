7f9135a36fe2f4f7d7aee9e3e960292ca3944b1b
Dan Chin <danielmchin@outlook.com>
2021-01-28 16:22:43 -0500

    search input handling

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,59 +1,75 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import './NavBar.css';
 import { BsSearch } from 'react-icons/bs'
 
 const NavBar = ({ setAuthenticated }) => {
+  const history = useHistory()
+
+  const [keyword, setKeyword] = useState("")
+  const [zipcode, setZipcode] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newSearch = {
+      keyword,
+      zipcode,
+    };
+    console.log(newSearch);
+    handleInputReset();
+  }
+
+  const handleInputReset = () => {
+    setKeyword("")
+    setZipcode("")
+  }
+
 	return (
-		// <nav>
-		//   <ul>
-		//     <li>
-		//       <NavLink to="/" exact={true} activeClassName="active">
-		//         Home
-		//       </NavLink>
-		//     </li>
-		//     <li>
-		//       <NavLink to="/login" exact={true} activeClassName="active">
-		//         Login
-		//       </NavLink>
-		//     </li>
-		//     <li>
-		//       <NavLink to="/sign-up" exact={true} activeClassName="active">
-		//         Sign Up
-		//       </NavLink>
-		//     </li>
-		//     <li>
-		//       <NavLink to="/users" exact={true} activeClassName="active">
-		//         Users
-		//       </NavLink>
-		//     </li>
-		//     <li>
-		//       <LogoutButton setAuthenticated={setAuthenticated} />
-		//     </li>
-		//   </ul>
-		// </nav>
-
 		<div className="navbarWrapper">
+      <div className="header_left">
+        <div className="home">
+          <img className="header-logo"
+          onClick={() => history.push('/')}
+          src={"react-app/logo.png"} />
+        </div>
 
-			<div className="home">
-				<NavLink to="/" exact={true} activeClassName="active">
-					Home
-				</NavLink>
-			</div>
-
-
-      <div className="searchBar">
-        <input type='text' defaultValue='Search for keywords' />
-        <input type='text' defaultValue='City or Zip Code' />
-        <BsSearch />
+        <div className="searchBar">
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              value={keyword}
+              onChange={(event) => setKeyword(event.target.value)}
+              placeholder='Search by keyword'
+            />
+            <input
+              type='text'
+              value={zipcode}
+              onChange={(event) => setZipcode(event.target.value)}
+              placeholder='Zip Code'
+              defaultValue='78704'
+            />
+            <button className='searchButton' type='submit'>
+              <BsSearch />
+            </button>
+          </form>
+        </div>
       </div>
 
-      <div className="userWrapper">
+      <div className="header_right">
+        <div className='start_new_group'>
+          <NavLink to='/new-group' exact={true} activeClassName="active">
+            Start a new group
+          </NavLink>
+        </div>
         <div className="login">
           <NavLink to="/login" exact={true} activeClassName="active">
             Login
           </NavLink>
+        </div>
+
+        <div className="logoutButton">
+          <LogoutButton setAuthenticated={setAuthenticated} />
         </div>
 
         <div className="signup">
@@ -61,12 +77,8 @@ const NavBar = ({ setAuthenticated }) => {
             Sign Up
           </NavLink>
 
-        <div className="logoutButton">
-          <LogoutButton setAuthenticated={setAuthenticated} />
-        </div>
         </div>
       </div>
-
 		</div>
 	);
 };
