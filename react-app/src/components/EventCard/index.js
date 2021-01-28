60344197844ef4d46a6112c7ee3f671af30959cf
percist <percist@gmail.com>
2021-01-28 13:43:19 -0500

    Attendee card needs testing

--- react-app/src/components/EventCard/index.js
+++ react-app/src/components/EventCard/index.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import EventPage from "../EventPage";
-import { fetchAllUsers } from "../../store/users";
-import { BsCameraVideo, BsGeoAlt } from 'react-icons/bs';
+import { fetchAllEventUsers } from "../../store/users";
+import { BsCameraVideo, BsGeoAlt, BsPerson } from 'react-icons/bs';
+import RSVP from "../RSVP"
 import "./index.css";
+import UserImage from "../UserImage";
 
 const event = {
   id: 23,
@@ -26,26 +28,33 @@ const event = {
 };
 
 const EventCard = ({  }) => {
+  const dispatch = useDispatch();
 
   const users = useSelector(reduxState => {
-    return reduxState.users
+    return reduxState.users;
   })
 
   useEffect( () =>{
-    dispatch(fetchAllUsers(event.eventId))
+    dispatch(fetchAllEventUsers(event.eventId));
   },[dispatch])
 
   return (
     <div className="event-card">
       <div className="event-card_date">{event.start_time}</div>
       <div className="event-card_title">{event.event_name}</div>
-      <BsCameraVideo />
       <div className="event-card_location">
         {event.virtual ? <><BsCameraVideo /><p>Virtual event</p></> : <><div><BsGeoAlt />{event.address}</div><div>{event.city}, {event.state} {event.zip_code}</div></>}
       </div>
       <div className="event-card_description">{event.description}</div>
-      <div className="event-card_attendees">127 Attendees</div>
-      <>RSVP COMPONENT</>
+      <div className="event-card_attendees">
+        <div className="event-card_attendees_pics">
+        {users.length > 0 ? users.slice(0, 2).map(user => <UserImage image={user.image_url} />) : <BsPerson />}
+        </div>
+        <div className="event-card_attendees_total">
+          {users.length > 0 ? `${users.length} going` : "1 going"}
+        </div>
+      </div>
+      <RSVP />
     </div>
   );
 }
