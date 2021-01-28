c4fcc3a37d106d46ce61fa5524f548f803cc9bdc
percist <percist@gmail.com>
2021-01-28 10:31:23 -0500

    Add random comments, edit to include virtual events

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -1,9 +1,12 @@
 from app.models import db, Comment
+from faker import Faker
+from random import random
 
 # Adds a demo user, you can add other users here if you want
 
 
 def seed_comments():
+    fakes = Faker()
 
     demo_comment1 = Comment(
         user_id=2, event_id=1, body='This event is wack. This is a workshop happening outdoors...for coding.')
@@ -37,6 +40,17 @@ def seed_comments():
     db.session.add(demo_comment4)
 
     db.session.commit()
+
+    for _ in range (200):
+        random_length = int(random() * 3 ) + 1
+        random_user = int(random() * 299 ) + 1
+        random_event = int(random() * 399 ) + 1
+
+        random_comment = Comment( user_id=random_user, event_id=random_event, 
+                                body=fakes.paragraph(nb_sentences=random_length, variable_nb_sentences=True))
+        db.session.add(random_comment)
+
+        db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
