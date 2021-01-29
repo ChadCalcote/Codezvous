647d7bc806d1c0ea2aa4cbd8690a599669853f3a
percist <percist@gmail.com>
2021-01-29 12:23:45 -0500

    sonly displaying ALL groups

---
+++ react-app/src/components/GroupGallery/index.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAllGroups } from '../../store/groups';
+import { getCurrentUser } from '../../store/session';
+import { BsCameraVideo, BsGeoAlt, BsPerson } from 'react-icons/bs';
+import GroupCard from '../GroupCard'
+import UserImage from '../UserImage';
+import './GroupGallery.css'
+
+const GroupGallery = ({ groupType }) => {
+    const dispatch = useDispatch();
+
+    const groups = useSelector(reduxState => {
+        return reduxState.groups
+    })
+
+    const user = useSelector(reduxState => {
+        return reduxState.session
+    })
+
+    useEffect(() => {
+        dispatch(fetchAllGroups())
+        dispatch(getCurrentUser())
+    }, [dispatch])
+
+// Need to figure out how to display either groups the currently authenticated user belongs to
+// OR all groups
+
+    const renderSuggestedGroups = () => {
+        const nonUserGroups = groups.filter(groups => user.)
+        groups.slice(0,20).map(group => <GroupCard group={group} key={group.id} />)
+    }
+
+    return (
+        <div className="group-suggested-gallery">
+            {!groups && <h3>Loading....</h3>}
+            {/* {Array.isArray(sectionEvents) && sectionEvents.map(event => <EventCard event={event} key={event.id} />)} */} */}
+            {Array.isArray(groups) && groups.slice(0,20).map(group => <GroupCard group={group} key={group.id} />)}
+        </div>
+    )
+}
+
+export default GroupGallery;
--- react-app/src/components/UserImage/index.js
+++ react-app/src/components/UserImage/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { fetchSingleUser } from "../../store/users";
@@ -16,24 +16,30 @@ const handleImageLoaded = () => setImageLoaded("loaded")
 
 const handleImageErrored = () => setImageErrored("failed to load")
 
+function displayUserImage (user){
+  return (
+    <img width="100px" 
+      src={user.image_url}
+      onLoad={handleImageLoaded}
+      onError={handleImageErrored}
+    />
+  )
+}
+
 // // Fetch User
 // const user = useSelector(reduxState => {
 //   return reduxState.users
 // })
 
-// useEffect(() => {
-//   dispatch(fetchSingleUser(userId))
+  useEffect(() => {
+    displayUserImage(user)
 
-// }, [dispatch])
+  }, [user])
 // // Use User's image 
 
     return (
       <div className="user_photo">
-        {user.image_url ? <img width="100px" 
-                                src={user.image_url}
-                                onLoad={handleImageLoaded}
-                                onError={handleImageErrored}
-                                /> : null
+        {user.image_url ?  displayUserImage(user) : null
         }
       </div>
     );
