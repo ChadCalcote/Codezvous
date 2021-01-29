8d61b8837bcf076c8952ba802667f26716d6166e
Dan Chin <danielmchin@outlook.com>
2021-01-29 10:58:29 -0500

    fixing margin inconsistency and package-lock

--- react-app/src/components/Dropdown/index.js
+++ react-app/src/components/Dropdown/index.js
@@ -1,52 +1,43 @@
 import React, { useState, useRef } from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
-import { useDetectOutsideClick } from "./useDetectOutsideClick";
+import { useDetectOutsideClick } from './useDetectOutsideClick';
 import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
 import { CgProfile } from 'react-icons/cg';
 import './Dropdown.css';
 
-const Dropdown = ({setAuthenticated}) => {
-
+const Dropdown = ({ setAuthenticated }) => {
 	const dropdownRef = useRef(null);
-  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
+	const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
 	const onClick = () => setIsActive(!isActive);
 
 	return (
 		<div className="menu-container">
-					<i className="menu_icon" onClick={onClick}>
-						<CgProfile />
-						{isActive ? <BsChevronUp /> : <BsChevronDown />}
-					</i>
-					<nav
-						ref={dropdownRef}
-						className={`menu ${isActive ? 'active' : 'inactive'}`}
-					>
-						<ul>
-							<li className="nav-item">
-								<NavLink to="/">
-									Home
-								</NavLink>
-							</li>
-							<li className="nav-item">
-								<NavLink to="/groups/1">
-									Demo Groups
-								</NavLink>
-							</li>
-							<li className="nav-item">
-								<NavLink to="/events/1">
-									Demo Events
-								</NavLink>
-							</li>
-							<li className="nav-item">
-								<NavLink to="/signup">
-									Sign Up
-								</NavLink>
-							</li>
-						</ul>
-					</nav>
-				</div>
+			<i className="menu_icon" onClick={onClick}>
+				<CgProfile />
+				{isActive ? <BsChevronUp /> : <BsChevronDown />}
+			</i>
+			<nav
+				ref={dropdownRef}
+				className={`menu ${isActive ? 'active' : 'inactive'}`}
+			>
+				<ul>
+					<li className="nav-item">
+						<NavLink to="/">Home</NavLink>
+					</li>
+					<li className="nav-item">
+						<NavLink to="/groups/1">Demo Groups</NavLink>
+					</li>
+					<li className="nav-item">
+						<NavLink to="/events/1">Demo Events</NavLink>
+					</li>
+					<li className="nav-item">
+						<NavLink to="/signup">Sign Up</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	);
 };
 
-export default Dropdown
+export default Dropdown;
--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -2,23 +2,22 @@ import React from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
 import SearchBar from '../SearchBar';
 import Dropdown from '../Dropdown';
-import logo from './logo.png'
+import logo from './logo.png';
 
 import './NavBar.css';
 
 const NavBar = ({ setAuthenticated }) => {
 	const history = useHistory();
 
-
 	return (
 		<div className="navbarWrapper">
 			<div className="header_left">
 				<div className="home">
-            <img
-              className="header_logo"
-              onClick={() => history.push('/')}
-              src={logo}
-            />
+					<img
+						className="header_logo"
+						onClick={() => history.push('/')}
+						src={logo}
+					/>
 				</div>
 				<div className="searchbar">
 					<SearchBar />
@@ -31,8 +30,7 @@ const NavBar = ({ setAuthenticated }) => {
 						Start a new group
 					</NavLink>
 				</div>
-        <Dropdown />
-
+				<Dropdown />
 			</div>
 		</div>
 	);
