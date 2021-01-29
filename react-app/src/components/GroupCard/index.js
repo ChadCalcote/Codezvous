c14f02ecb9d2f36216b8014b478c60fdabdceab4
Chad Calcote <calcote@att.net>
2021-01-28 19:42:28 -0700

    One more change

--- react-app/src/components/GroupCard/index.js
+++ react-app/src/components/GroupCard/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchGroupUsers } from "../../store/users";
 import "./index.css";
