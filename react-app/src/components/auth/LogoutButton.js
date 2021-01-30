e0b8ab0dc9f820f57dad0a3fe94e7916f62cf258
Dan Chin <danielmchin@outlook.com>
2021-01-30 17:03:39 -0500

    cleaned up

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { logout } from "../../services/auth";
-// import { useHistory } from 'react-router-dom';
 
 
 const LogoutButton = ({setAuthenticated}) => {
-  // const history = useHistory();
 
   const onLogout = async (e) => {
     await logout();
