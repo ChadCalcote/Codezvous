ccc2b6f6e9e5fbafc8735cffd73a4d4746a5c3ac
jimmy <demeatricej.sherrod@gmail.com>
2021-02-01 00:40:08 -0600

    creating new event redirects you to previous page after submission

--- react-app/src/components/EventForm/index.js
+++ react-app/src/components/EventForm/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./index.css";
 
 const createEvent = async (event_name, description, address, city, state, zip_code, virtual, type, image_url, group_id, start_time, end_time, status) => {
@@ -25,9 +26,9 @@ const createEvent = async (event_name, description, address, city, state, zip_co
     });
     return await response.json();
 };
-
 const EventFormReact = () => {
-
+    
+    const history = useHistory()
     const [eventName, setEventName] = useState("");
     const [description, setDescription] = useState("");
     const [address, setAddress] = useState("");
@@ -58,9 +59,11 @@ const EventFormReact = () => {
         setStartTime("")
         setEndTime("")
         setStatus("Ongoing")
+        history.goBack()
     }
 
     return (
+       <> 
         <form onSubmit={onSubmit}>
             <div>
                 <label>Event Name</label>
@@ -195,6 +198,8 @@ const EventFormReact = () => {
             </div>
             <button type="submit">Create Event</button>
       </form>
+      
+     </> 
     );
 };
 
