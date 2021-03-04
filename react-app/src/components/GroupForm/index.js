496356c3d8acc1b6fd68757bdf1d74c8279a72b1
percist <percist@gmail.com>
2021-03-04 14:31:07 -0500

    updated group form to automatically pass in user as leader

--- react-app/src/components/GroupForm/index.js
+++ react-app/src/components/GroupForm/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { useSelector } from 'react-redux';
 import "./GroupForm.css";
 
 
@@ -11,9 +12,10 @@ const GroupForm = () => {
   const [state, setState] = useState("");
   const [zipCode, setZipCode] = useState();
   const [imageUrl, setImageUrl] = useState("");
-  const [leader_id, setLeaderId] = useState();
-  
-  const createGroup = async (group_name, description, city, state, zip_code, image_url, leader_id) => {
+  const currentUser = useSelector(state => state.session);
+
+  const createGroup = async (group_name, description, city, state, zip_code, image_url) => {
+    const leader_id = currentUser.id
     const response = await fetch("/api/groups/test", {
       method: "POST",
       headers: {
@@ -32,103 +34,91 @@ const GroupForm = () => {
     return await response.json();
   };
 
-    const onSubmit = async(event) => {
-        event.preventDefault();
-        const group = await createGroup(groupName, description, city, state, zipCode, imageUrl, leader_id);
-        setGroupName("");
-        setDescription("");
-        setCity("");
-        setState("");
-        setZipCode(0);
-        setImageUrl("");
-        setLeaderId(0);
-        history.push(`/groups/${group.id}`);
-    };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const group = await createGroup(groupName, description, city, state, zipCode, imageUrl);
+    setGroupName("");
+    setDescription("");
+    setCity("");
+    setState("");
+    setZipCode(0);
+    setImageUrl("");
+    history.push(`/groups/${group.id}`);
+  };
 
-    return (
-      <form className="group-form" onSubmit={onSubmit}>
-        <div className="form-wrapper">
-          <div>
-            <label>Group Name</label>
-            <br />
-            <input
-              type="text"
-              name="groupName"
-              onChange={(event) => setGroupName(event.target.value)}
-              value={groupName}
-            ></input>
-          </div>
-          <br />
-          <div>
-            <label>Description</label>
-            <br />
-            <textarea
-              className="description"
-              type="text"
-              name="description"
-              onChange={(event) => setDescription(event.target.value)}
-              value={description}
-            ></textarea>
-          </div>
-          <br />
-          <div>
-            <label>City</label>
-            <br />
-            <input
-              type="text"
-              name="city"
-              onChange={(event) => setCity(event.target.value)}
-              value={city}
-            ></input>
-          </div>
-          <br />
-          <div>
-            <label>State</label>
-            <br />
-            <input
-              type="text"
-              name="state"
-              onChange={(event) => setState(event.target.value)}
-              value={state}
-            ></input>
-          </div>
-          <br />
-          <div>
-            <label>Zip Code</label>
-            <br />
-            <input
-              name="zipCode"
-              onChange={(event) => setZipCode(event.target.value)}
-              value={zipCode}
-            ></input>
-          </div>
-          <br />
-          <div>
-            <label>Image URL</label>
-            <br />
-            <input
-              type="text"
-              name="imageUrl"
-              onChange={(event) => setImageUrl(event.target.value)}
-              value={imageUrl}
-            ></input>
-          </div>
-          <br />
-          <div>
-            <label>Leader</label>
-            <br />
-            <input
-              type="number"
-              name="leader_id"
-              onChange={(event) => setLeaderId(event.target.value)}
-              value={leader_id}
-            ></input>
-          </div>
-          <br />
-            <button type="submit" className="group-form-submit-button">Create Group</button>
+  return (
+    <form className="group-form" onSubmit={onSubmit}>
+      <div className="form-wrapper">
+        <div className="group-form-label">
+          <label>Group Name</label>
         </div>
-      </form>
-    );
+        <div className="group-form-input">
+          <input
+            type="text"
+            name="groupName"
+            onChange={(event) => setGroupName(event.target.value)}
+            value={groupName}
+          />
+        </div>
+        <div className="group-form-label">
+          <label>Description</label>
+        </div>
+        <div className="group-form-input">
+          <textarea
+            className="description"
+            type="text"
+            name="description"
+            onChange={(event) => setDescription(event.target.value)}
+            value={description}
+          />
+        </div>
+        <div className="group-form-label">
+          <label>City</label>
+        </div>
+        <div className="group-form-input">
+          <input
+            type="text"
+            name="city"
+            onChange={(event) => setCity(event.target.value)}
+            value={city}
+          />
+        </div>
+        <div className="group-form-label">
+          <label>State</label>
+        </div>
+        <div className="group-form-input">
+          <input
+            type="text"
+            name="state"
+            onChange={(event) => setState(event.target.value)}
+            value={state}
+          />
+        </div>
+        <div className="group-form-label">
+          <label>Zip Code</label>
+        </div>
+        <div className="group-form-input">
+          <input
+            name="zipCode"
+            onChange={(event) => setZipCode(event.target.value)}
+            value={zipCode}
+          />
+        </div>
+        <div className="group-form-label">
+          <label>Image URL</label>
+        </div>
+        <div className="group-form-input">
+          <input
+            type="text"
+            name="imageUrl"
+            onChange={(event) => setImageUrl(event.target.value)}
+            value={imageUrl}
+          />
+        </div>
+        <button type="submit" className="group-form-submit-button">Create Group</button>
+      </div>
+    </form>
+  );
 };
 
 export default GroupForm;
