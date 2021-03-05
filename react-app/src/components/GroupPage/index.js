3294ae88a905e7370134c9ec54925e27c566a860
percist <percist@gmail.com>
2021-03-05 16:20:15 -0500

    page styling

--- react-app/src/components/GroupPage/index.js
+++ react-app/src/components/GroupPage/index.js
@@ -13,81 +13,82 @@ import UserImage from '../UserImage';
 import "./GroupPage.css"
 
 const GroupPage = () => {
-    const params = useParams();
-    const dispatch = useDispatch();
+  const params = useParams();
+  const dispatch = useDispatch();
 
-    const { groupId } = params;
+  const { groupId } = params;
 
-    const [leader, setLeader] = useState({});
-    const [isLeader, setIsLeader] = useState(false);
-    const [activeUser, setActiveUser] = useState({});
-    const [isMember, setIsMember] = useState(false);
-    const [members, setMembers] = useState([]);
-    const [userGroupState, setUserGroupState] = useState([])
+  const [leader, setLeader] = useState({});
+  const [isLeader, setIsLeader] = useState(false);
+  const [activeUser, setActiveUser] = useState({});
+  const [isMember, setIsMember] = useState(false);
+  const [members, setMembers] = useState([]);
+  const [userGroupState, setUserGroupState] = useState([])
 
-    // If session.userId === leader.id make edit/delete/add buttons available
-    const group = useSelector(state => state.groups); // Returning an Object
-    const groupUsers = useSelector(state => state.users);// Returning a list
-    const currentUser = useSelector(state => state.session );
-    const events = useSelector(state => state.events);
-    const groupLeaderId = group.leader_id;
+  // If session.userId === leader.id make edit/delete/add buttons available
+  const group = useSelector(state => state.groups); // Returning an Object
+  const groupUsers = useSelector(state => state.users);// Returning a list
+  const currentUser = useSelector(state => state.session);
+  const events = useSelector(state => state.events);
+  const groupLeaderId = group.leader_id;
 
-    const onClick = async (event) => {
-      event.preventDefault();
-      const newUserGroup = {
-        user_id: currentUser.id,
-        group_id: parseInt(groupId),
-      };
-
-      dispatch(createUserGroup(newUserGroup));
-      setUserGroupState(newUserGroup);
-      setIsMember(true);
-      dispatch(fetchGroupUsers(groupId));
+  const onClick = async (event) => {
+    event.preventDefault();
+    const newUserGroup = {
+      user_id: currentUser.id,
+      group_id: parseInt(groupId),
     };
 
-    useEffect(() => {
-      if (Array.isArray(groupUsers)) {
-        setLeader(groupUsers.find((user) => {
-          return user.id === groupLeaderId;
-        }))
-        setIsLeader(currentUser.id === groupLeaderId )
-        setMembers(groupUsers);
-      }
-    }, [currentUser, groupUsers, groupLeaderId]);
+    dispatch(createUserGroup(newUserGroup));
+    setUserGroupState(newUserGroup);
+    setIsMember(true);
+    dispatch(fetchGroupUsers(groupId));
+  };
 
-    useEffect(() => {
-      if (currentUser) {
-        setActiveUser(currentUser)
-      }
-    }, [currentUser]);
+  useEffect(() => {
+    if (Array.isArray(groupUsers)) {
+      setLeader(groupUsers.find((user) => {
+        return user.id === groupLeaderId;
+      }))
+      setIsLeader(currentUser.id === groupLeaderId)
+      setMembers(groupUsers);
+    }
+  }, [currentUser, groupUsers, groupLeaderId]);
 
-    useEffect(() => {
-      if (Array.isArray(groupUsers)) {
+  useEffect(() => {
+    if (currentUser) {
+      setActiveUser(currentUser)
+    }
+  }, [currentUser]);
 
-        for (let i = 0; i < groupUsers.length; i++) {
-          if (groupUsers[i].id === currentUser.id) {
-            setIsMember(true)
-            return;
-          }
+  useEffect(() => {
+    if (Array.isArray(groupUsers)) {
+
+      for (let i = 0; i < groupUsers.length; i++) {
+        if (groupUsers[i].id === currentUser.id) {
+          setIsMember(true)
+          return;
         }
       }
-    }, [groupUsers, currentUser]);
+    }
+  }, [groupUsers, currentUser]);
 
-    const numGroupUsers = groupUsers.length;
+  const numGroupUsers = groupUsers.length;
 
-    useEffect(() => {
-      dispatch(fetchOneGroup(groupId));
-      dispatch(fetchGroupUsers(groupId));
-      dispatch(getCurrentUser());
-      dispatch(fetchGroupEvents(groupId));
-    }, [dispatch, groupId])
+  useEffect(() => {
+    dispatch(fetchOneGroup(groupId));
+    dispatch(fetchGroupUsers(groupId));
+    dispatch(getCurrentUser());
+    dispatch(fetchGroupEvents(groupId));
+  }, [dispatch, groupId])
 
-    return (
-      <div className="group-page">
-        <div className="group-header">
+  return (
+    <div className="group-page">
+      <div className="group-header">
+        <div className="group-header-content">
           <div className="group-header_img">
-            {!group.image_url && <img src="../../../Bars-0.7s-98px.gif"  alt="loading..."/>}
-            {group && <img id="group-header_img" src={group.image_url} alt="group"/>}
+            {!group.image_url && <img src="../../../Bars-0.7s-98px.gif" alt="loading..." />}
+            {group && <img id="group-header_img" src={group.image_url} alt="group" />}
           </div>
           <div className="group-header_info">
             <div id="group-header_info_title">
@@ -100,52 +101,53 @@ const GroupPage = () => {
               <BsPeople />  {`${numGroupUsers} members`}
             </div>
             <div id="group-header_info_leader">
-              <BsPerson /> Organized by leader:   
+              <BsPerson /> Organized by leader:
               {leader ? ` ${leader.username}` : null}
             </div>
             <div id="group-header_info_member_button">
-              {isLeader ? 
+              {isLeader ?
                 <>
-                  <h2>You're the leader</h2> 
+                  <h2>You're the leader</h2>
                   <div className="create-event-button">
                     <Link to="/groups/create/event">Create Event</Link>
                   </div>
-                </> 
-                : isMember ? 
+                </>
+                : isMember ?
                   <h2>You're a member</h2> : <button className="membership-button" onClick={onClick}>Join Us!</button>
               }
             </div>
           </div>
         </div>
-        <div className="group-body">
-          <div className="group-body_feed">
-            <div id="group-body_feed_description">
-              <h2> What we're about</h2>
-              {group.description}
-            </div>
-            <div id="group-body_feed_events">
-              <h2> Upcoming Events ({`${events.length}`})</h2> 
-              <EventGallery events={events} parent={"groupPage"} user={currentUser}/>
-            </div>
+      </div>
+      <div className="group-body">
+        <div className="group-body_feed">
+          <div id="group-body_feed_description">
+            <h2> What we're about</h2>
+            {group.description}
           </div>
-          <div className="group-body_sidebar">
-            <div className="group-body_sidebar_header">
-              <div id="group-body_sidebar_organizer">
-                <h3>Organizer</h3>
-              </div>
-              {leader ? <UserImage user={leader}/>: null}
-              <div id="group-body_sidebar_members">Member icons</div>
-                <h3>Members ({`${members.length}`})</h3>
-              </div>
-              <div id="group-body_sidebar_gallery">
-                {members.length > 0 &&
-                Array.isArray(members) &&
-                members.slice(0, 12).map((user) => <UserImage user={user} key={user.id} />)}
-              </div>
-              </div>
+          <div id="group-body_feed_events">
+            <h2> Upcoming Events ({`${events.length}`})</h2>
+            <EventGallery events={events} parent={"groupPage"} user={currentUser} />
+          </div>
+        </div>
+        <div className="group-body_sidebar">
+          <div className="group-body_sidebar_header">
+            <div id="group-body_sidebar_organizer">
+              <h3>Organizer</h3>
+            </div>
+            {leader ? <UserImage user={leader} /> : null}
+            <div id="group-body_sidebar_members">Member icons</div>
+            <h3>Members ({`${members.length}`})</h3>
+          </div>
+          <div id="group-body_sidebar_gallery">
+            {members.length > 0 &&
+              Array.isArray(members) &&
+              members.slice(0, 12).map((user) => <UserImage user={user} key={user.id} />)}
+          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default GroupPage;
