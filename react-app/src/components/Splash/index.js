08de888aee38792d3da78d0e33a20b2473588516
percist <percist@gmail.com>
2021-03-08 17:13:13 -0500

    video

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -8,13 +8,16 @@ const Splash = () => {
 
   return (
     <div className="splash-page-wrapper">
-      <p>**placeholder for the video on repeat?**</p>
-      <br />
-      <h1>Welcome to Codezvous.</h1>
-      <h4>" Find the events so you can do more of what matters to you. Or create your own group and meet people near you who share your interests. "</h4>
-      <div className="splash-page_buttons">
-        <button onClick={() => history.push('/login')}>Click Here to Begin</button>
-        <a href="/signup">Don't have an account?</a>
+      <div className="splash-page-video-container">
+        <video className="header-video" autoplay="true" loop="true" src="https://www.meetup.com/mu_static/en-US/video.dddafbfe.mp4" />
+      </div>
+      <div className="splash-page-overlay">
+        <h1>Welcome to Codezvous.</h1>
+        <h4>" Find the events so you can do more of what matters to you. Or create your own group and meet people near you who share your interests. "</h4>
+        <div className="splash-page_buttons">
+          <button onClick={() => history.push('/login')}>Click Here to Begin</button>
+          <a href="/signup">Don't have an account?</a>
+        </div>
       </div>
     </div>
 
